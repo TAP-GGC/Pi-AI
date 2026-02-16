489c1de1106bf79ebeb7fb27c4df0edf0479ee44
kyth-ggc <kthorpe1@ggc.edu>
2026-02-16 17:12:21 -0500

    task list

--- code/app.js
+++ code/app.js
@@ -49,3 +49,42 @@ start.addEventListener("click", startTimer);
 stop.addEventListener("click", stopTimer);
 reset.addEventListener("click", resetTimer);
 
+const taskForm= document.getElementById("task-form");
+const taskList= document.getElementById("task-list");
+
+//Counter variable to track task index
+
+let taskIndex = 1;
+
+    //console.log(taskForm,taskList);
+
+    taskForm.addEventListener("submit", function(event){
+        event.preventDefault();
+    
+        const taskInput = document.getElementById("task-input");
+        const taskText = taskInput.value.trim();
+
+        //console.log(taskText);
+
+        if (taskText !==""){
+            //Creating new task item
+
+            const taskItem = document.createElement("li");
+            taskItem.classList.add("task-item");
+            taskItem.textContent = `${taskIndex}- ${taskText}`;
+        
+        taskItem.addEventListener("click", function () {
+            console.log("completed");
+            this.classList.toggle("completed");
+        });
+        
+            //append item to list
+        taskList.appendChild(taskItem);
+
+
+        //incrementing counter
+        taskIndex++;
+
+        taskInput.value = "";
+        }
+    });
